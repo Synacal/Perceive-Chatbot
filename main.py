e1d30c78ed9e233e758afac51c4856fe852b7bb8
Lahiru Wijesinghe <wml.eranga@gmail.com>
2024-05-29 02:55:55 +0530

    add test api

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from app.api.api_v1.endpoints import (
     prior_art_search,
     attachments,
     ip_validity_analysis,
+    test,
 )
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -22,3 +23,5 @@ app.include_router(answers.router)
 app.include_router(prior_art_search.router)
 app.include_router(attachments.router)
 app.include_router(ip_validity_analysis.router)
+
+app.include_router(test.router)
