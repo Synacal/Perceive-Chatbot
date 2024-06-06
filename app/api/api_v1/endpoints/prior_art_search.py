919586732949a3fe1d5a13e937df3b62f6924fda
Lahiru Wijesinghe <wml.eranga@gmail.com>
2024-06-06 19:52:36 +0530

    remove pinecoin

--- app/api/api_v1/endpoints/prior_art_search.py
+++ app/api/api_v1/endpoints/prior_art_search.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter, HTTPException, Depends
 from typing import List
 from app.models.prior_art_search import SearchQuery, PatentAnalysis
+
 # from app.models.document import Document  # Assuming Document is a Pydantic model for your database entries
-from app.services.prior_art_search import search_documents, search_patents
+from app.services.prior_art_search import search_documents
 
 router = APIRouter()
+
 
 @router.post("/keyword-search")
 async def keyword_search(query: SearchQuery):
@@ -15,7 +17,9 @@ async def keyword_search(query: SearchQuery):
         raise e
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-    
+
+
+"""  
 @router.post("/vector-search")
 async def keyword_search(query: PatentAnalysis):
     # try:
@@ -24,4 +28,4 @@ async def keyword_search(query: PatentAnalysis):
     # except HTTPException as e:
     #     raise e
     # except Exception as e:
-    #     raise HTTPException(status_code=500, detail=str(e))
+    #     raise HTTPException(status_code=500, detail=str(e))"""
--- app/services/prior_art_search.py
+++ app/services/prior_art_search.py
@@ -1,27 +1,31 @@
 from fastapi import APIRouter, HTTPException, Depends
 from typing import List
 from app.models.prior_art_search import SearchQuery
+
 # from app.models.document import Document  # Assuming Document is a Pydantic model for your database entries
 from app.core.database import get_percieve_db_connection
 from psycopg2 import sql
 from app.models.prior_art_search import PatentAnalysis, PatentResult
-from app.utils.prior_art_search_helpers import vectorize_description, query_pinecone_index, generate_analysis
+from app.utils.prior_art_search_helpers import vectorize_description, generate_analysis
+
 
 async def search_documents(query: SearchQuery):
     try:
         conn = get_percieve_db_connection()
         # Properly split and prepare the keywords for the tsquery
         # Using ' & ' for AND logic in full-text search to ensure broader results
-        query_keywords = ' | '.join(query.keywords) 
+        query_keywords = " | ".join(query.keywords)
         ts_query = sql.SQL("plainto_tsquery('english', %s)")
 
         # Construct the full-text search query using parameterized SQL
-        query = sql.SQL("""
+        query = sql.SQL(
+            """
             SELECT id, title, abstract, claim_text
             FROM target.patents
             WHERE (title_abstract_tsvector @@ {ts_query})
                OR (claims_tsvector @@ {ts_query});
-        """).format(ts_query=ts_query)
+        """
+        ).format(ts_query=ts_query)
         # print(query)
         # Execute the query
         with conn.cursor() as cur:
@@ -31,7 +35,8 @@ async def search_documents(query: SearchQuery):
     except Exception as e:
         print(f"Error executing search query: {e}")
         return []
-    
+
+    """
 async def search_patents(data: PatentAnalysis):
     description_vector = vectorize_description(data.description)
     query_results = query_pinecone_index(description_vector)
@@ -43,4 +48,4 @@ async def search_patents(data: PatentAnalysis):
         abstract = match['metadata']['abstract']
         content = generate_analysis(data.description, match['metadata'])
         results.append(PatentResult(id=patent_id, title=title, abstract=abstract, content=content))
-    return results
+    return results"""
