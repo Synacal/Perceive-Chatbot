ed8ec63ac3a1d1df4ef79086e7f36304202085ba
Nirodya Pussadeniya <en95142@sjp.ac.lk>
2024-06-08 15:00:56 +0530

    Add prior-art-search endpoint

--- app/api/api_v1/endpoints/prior_art_search.py
+++ app/api/api_v1/endpoints/prior_art_search.py
@@ -1,9 +1,14 @@
 from fastapi import APIRouter, HTTPException, Depends
 from typing import List
-from app.models.prior_art_search import SearchQuery, PatentAnalysis
+from app.services.get_answers import get_answers, get_all_answers_with_questions
+from app.models.prior_art_search import SearchQuery, PatentAnalysis, PriorArtSearch
 
 # from app.models.document import Document  # Assuming Document is a Pydantic model for your database entries
-from app.services.prior_art_search import search_documents
+from app.services.prior_art_search import (
+    search_documents,
+    search_patents,
+    generate_innovation_summary,
+)
 
 router = APIRouter()
 
@@ -19,13 +24,29 @@ async def keyword_search(query: SearchQuery):
         raise HTTPException(status_code=500, detail=str(e))
 
 
-"""
 @router.post("/vector-search")
-async def keyword_search(query: PatentAnalysis):
+async def vector_search(query: PatentAnalysis):
     # try:
-        response_data = await search_patents(query)
-        return response_data
+    response_data = await search_patents(query)
+    return response_data
+
+
+# except HTTPException as e:
+#     raise e
+# except Exception as e:
+#     raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.post("/prior-art-search")
+async def prior_art_search(query: PriorArtSearch):
+    # try:
+    answers = get_all_answers_with_questions(query.user_id, query.session_id)
+    # docs_filtered_by_keywords = await search_documents(query)
+    # print(docs_filtered_by_keywords)
+    summary = generate_innovation_summary(answers[:10])
+    response = {"summary": summary, "status": "in-progress"}
+    return response
     # except HTTPException as e:
     #     raise e
     # except Exception as e:
-    #     raise HTTPException(status_code=500, detail=str(e))"""
+    #     raise HTTPException(status_code=500, detail=str(e))
--- app/services/prior_art_search.py
+++ app/services/prior_art_search.py
@@ -7,6 +7,7 @@ from app.core.database import get_percieve_db_connection
 from psycopg2 import sql
 from app.models.prior_art_search import PatentAnalysis, PatentResult
 from app.utils.prior_art_search_helpers import vectorize_description, generate_analysis
+from app.core.azure_client import client
 
 
 async def search_documents(query: SearchQuery):
@@ -36,16 +37,67 @@ async def search_documents(query: SearchQuery):
         print(f"Error executing search query: {e}")
         return []
 
-    """
+
 async def search_patents(data: PatentAnalysis):
     description_vector = vectorize_description(data.description)
     query_results = query_pinecone_index(description_vector)
     results = []
 
-    for match in query_results['matches']:
-        patent_id = match['metadata']['id']
-        title = match['metadata']['title']
-        abstract = match['metadata']['abstract']
-        content = generate_analysis(data.description, match['metadata'])
-        results.append(PatentResult(id=patent_id, title=title, abstract=abstract, content=content))
-    return results"""
+    for match in query_results["matches"]:
+        patent_id = match["metadata"]["id"]
+        title = match["metadata"]["title"]
+        abstract = match["metadata"]["abstract"]
+        content = generate_analysis(data.description, match["metadata"])
+        results.append(
+            PatentResult(id=patent_id, title=title, abstract=abstract, content=content)
+        )
+    return results
+
+
+def generate_innovation_summary(qa_pairs):
+    """
+    Generates a summary of the user's innovation using Azure AI based on provided question and answer pairs.
+
+    Parameters:
+    - qa_pairs (list of dicts): A list of dictionaries containing 'question' and 'answer' keys.
+
+    Returns:
+    - str: A summary of the innovation.
+    """
+    # Create a single string from all question-answer pairs
+    prompt = f"""Summarize the key points of an innovation based on the following details. 
+    Following details are on user's responses from the database for the predefined set 
+    of questions relevant to their innovation.
+    - Summarize the product or technology that has been developed, emphasizing its purpose and target industry.
+    - Describe in detail the technical aspects and the unique features of the innovation. Highlight how these features contribute to novelty within its field.
+    - Explain the innovation's business model, focusing on primary and potential revenue streams.
+    - Outline the company’s strategy for patent filing, including geographic focus and any prior art or existing patents that have been identified.
+    - Discuss how the innovation meets the criteria for novelty and non-obviousness, which are crucial for IP validity
+
+    User provided answers for set of pre-defined questions: 
+    {qa_pairs}
+    
+    """
+
+    message_text = [
+        {"role": "system", "content": prompt},
+        {"role": "user", "content": str(qa_pairs)},
+    ]
+
+    completion = client.chat.completions.create(
+        model="gpt-35-turbo",
+        messages=message_text,
+        temperature=0.7,
+        max_tokens=800,
+        top_p=0.95,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=None,
+    )
+
+    content = completion.choices[0].message.content
+
+    # Extract the generated text from the response
+    # summary = response.get("choices", [{}])[0].get("text", "").strip()
+
+    return content
