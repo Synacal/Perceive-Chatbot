5e10e2f7f5fa6e5175dc3b48392200692b6737d0
Lahiru Wijesinghe <wml.eranga@gmail.com>
2024-06-09 18:56:11 +0530

    update_quickprompt

--- app/models/quickprompt.py
+++ app/models/quickprompt.py
@@ -4,6 +4,6 @@ from typing import Dict, Any, List
 
 class QuickPrompt(BaseModel):
     user_id: str
-    report_id: str
+    requirement_gathering_id: int
     prompt_data: List[Dict[str, Any]]
     content: str
