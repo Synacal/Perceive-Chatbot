3a72a29d68227eddee0c362e7c336b4d9f7334be
Lahiru Wijesinghe <wml.eranga@gmail.com>
2024-06-09 16:47:47 +0530

    uuid to shortuuid

--- app/services/requirements_gathering.py
+++ app/services/requirements_gathering.py
@@ -1,27 +1,29 @@
 from fastapi import HTTPException
 from app.models.requirements_gathering import RequirementsGathering
 from app.core.database import get_db_connection
-import uuid
+import shortuuid
 
 
 async def get_requirements_gathering(requirements: RequirementsGathering):
     query = """
-    INSERT INTO requirements_gathering (user_id, report_id,user_case_id)
-    VALUES %s
+    INSERT INTO requirements_gathering (user_id, report_id, user_case_id)
+    VALUES (%s, %s, %s)
     """
 
     try:
         conn = get_db_connection()
-        async with conn.transaction():
+        with conn.cursor() as cur:
             for user_case_id in requirements.user_case_ids:
-                # Generate a UUID for report_id
-                report_id = str(uuid.uuid4())
+                # Generate a unique ID for report_id using shortuuid
+                report_id = "PR" + shortuuid.uuid()
                 # Execute the query with the generated report_id
-                await conn.execute(
-                    query, (requirements.user_id, report_id, user_case_id)
+                cur.execute(
+                    query, (str(requirements.user_id), report_id, str(user_case_id))
                 )
+        conn.commit()
         return {"status": "success"}
     except Exception as e:
+        conn.rollback()
         raise HTTPException(status_code=500, detail=str(e))
     finally:
         conn.close()
